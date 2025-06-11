7bb18341379a1d71afdc30072d2b0c3dbf21b805
TOWICH2000 <qalaydada@gmail.com>
2025-06-11 06:50:49 +0300

    Update server.js

--- server.js
+++ server.js
@@ -194,6 +194,6 @@ app.get('/', (req, res) => {
 });
 
 const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
