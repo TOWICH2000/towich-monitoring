38df5ffbdc33bd73ecc09af15fb7a09da1bc102b
TOWICH2000 <qalaydada@gmail.com>
2025-06-11 06:37:54 +0300

    Update server.js

--- server.js
+++ server.js
@@ -193,8 +193,7 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-const PORT = process.env.PORT || 3001;  // Это правильно
-
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
